a2bf132c814a74eba48343e775d31a0977699e3c
Ulflander <xlaumonier@gmail.com>
2014-06-27 15:40:29 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-gulp-trycatch-closure
-=====================
+# gulp-trycatch-closure
 
-Enclose files content with a "try ... catch" statement
+> Enclose files content with a "try ... catch" statement
+
+## Usage
+
+First, install `gulp-trycatch-closure` as a development dependency:
+
+```shell
+npm install --save-dev gulp-trycatch-closure
+```
+
+Examples:
+
+```
+try {
+  /* content */
+} catch (e) {
+  console.warn(e);
+}
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+var through = require("through"),
+  gutil = require("gulp-util"),
+  Buffer = require("buffer").Buffer,
+  PluginError = gutil.PluginError,
+  File = gutil.File;
+
+module.exports = function () {
+  var file = null;
+
+  function write (f){
+    if (!f.isNull()) {
+      if (f.isStream()) {
+        this.emit("error", new PluginError("gulp-trycatch-closure",  "Streaming not supported"));
+      } else {
+        file = f;
+      }
+    }
+  }
+
+  function end () {
+    var newFile;
+
+    newFile = new File({
+      cwd: file.cwd,
+      base: file.base,
+      path: file.path,
+      contents: Buffer.concat([
+        new Buffer(";try {" + gutil.linefeed),
+        file.contents,
+        new Buffer(gutil.linefeed + "catch(e) { console.warn(e); };")
+      ])
+    });
+
+    this.emit("data", newFile);
+    this.emit("end");
+  }
+
+  return through(write, end);
+};
