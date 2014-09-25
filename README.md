a19ad593b1c9eec8e485cd96c15e81db8e21ae57
Ulflander <xlaumonier@gmail.com>
2014-09-25 11:11:26 +0200

    Better doc, add logger option, slight refactoring

--- README.md
+++ README.md
@@ -1,21 +1,68 @@
 # gulp-trycatch-closure
 
-> Enclose files content with a "try ... catch" statement
+Enclose JS files content with a "try ... catch" statement.
 
-## Usage
+It allows to easily catch errors on development.
 
-First, install `gulp-trycatch-closure` as a development dependency:
+## Install
+
+Install `gulp-trycatch-closure` using NPM, as a development dependency:
 
 ```shell
 npm install --save-dev gulp-trycatch-closure
 ```
 
-Examples:
+## Usage
 
+```js
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    trycatch = require('gulp-trycatch-closure');
+
+gulp.task('build', function() {
+    // Get all JS files
+    return gulp.src(['src/js/*.js'])
+        // Enclose their content with a try ... catch statement
+        .pipe(trycatch())
+        // Concat
+        .pipe(concat('build.js'))
+        // Send to destination
+        .pipe(gulp.dest('dist/'));
+});
 ```
+
+## Usage with gulp-if
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    ifif = require('gulp-if'),
+    trycatch = require('gulp-trycatch-closure'),
+
+    // Check if we're in dev env
+    isDev = process.NODE_ENV !== 'production';
+
+gulp.task('build', function() {
+    return gulp.src(['src/js/*.js'])
+        .pipe(ifif(isDev, trycatch()))
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('dist/'));
+});
+```
+
+## Change the way to log errors
+
+You can pass to the `trycatch` function an object of options. Here are the available options with their default value:
+
+* `logger` - Way to log the error, default is `console.warn(e);`
+
+## Example of resulting build file:
+
+```js
 try {
-  /* content */
+  
+    /* file content */
+
 } catch (e) {
-  console.warn(e);
+    console.warn(e);
 }
 ```
--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-var through = require("through"),
-  gutil = require("gulp-util"),
-  Buffer = require("buffer").Buffer,
-  PluginError = gutil.PluginError,
-  File = gutil.File;
+var through = require('through'),
+    gutil = require('gulp-util'),
+    Buffer = require('buffer').Buffer,
+    PluginError = gutil.PluginError,
+    File = gutil.File,
+    extend = require('util')._extend;
 
-module.exports = function () {
-  var file = null;
+module.exports = function (options) {
+    var file = null;
 
-  function write (f){
-    if (!f.isNull()) {
-      if (f.isStream()) {
-        this.emit("error", new PluginError("gulp-trycatch-closure",  "Streaming not supported"));
-      } else {
-        file = f;
-        var newFile;
+    function write (f){
+        if (!f.isNull()) {
+            if (f.isStream()) {
+                this.emit('error', new PluginError('gulp-trycatch-closure',  
+                    'Streaming not supported'));
+            } else {
+                file = f;
+                var newFile;
 
-        newFile = new File({
-          cwd: file.cwd,
-          base: file.base,
-          path: file.path,
-          contents: Buffer.concat([
-            new Buffer(";try {" + gutil.linefeed),
-            file.contents,
-            new Buffer(gutil.linefeed + "} catch(e) { console.warn(e); };")
-          ])
-        });
+                options = extend({
+                    logger: 'console.warn(e);'
+                }, options);
 
-        this.emit("data", newFile);
-      }
+                newFile = new File({
+                    cwd: file.cwd,
+                    base: file.base,
+                    path: file.path,
+                    contents: Buffer.concat([
+                        new Buffer(';try {' + gutil.linefeed),
+                        file.contents,
+                        new Buffer(gutil.linefeed + '} catch(e) { ' +
+                            options.logger + ' };')
+                    ])
+                });
+
+                this.emit('data', newFile);
+            }
+        }
     }
-  }
 
-  function end () {
-    this.emit("end");
-  }
+    function end () {
+        this.emit('end');
+    }
 
-  return through(write, end);
+    return through(write, end);
 };
